e70e4ceeac54a19b3444a7588cd94140d829173d
kathleenfs <kathleenfss4@gmail.com>
2026-08-17 20:26:39 -0300

    feat: configure OpenAPI and Swagger security

--- src/main/java/io/github/kathleenfs/productmanagementapi/config/SecurityConfig.java
+++ src/main/java/io/github/kathleenfs/productmanagementapi/config/SecurityConfig.java
@@ -41,7 +41,10 @@ public class SecurityConfig {
 
                         .requestMatchers(
                                 "/auth/register",
-                                "/auth/login"
+                                "/auth/login",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
                         ).permitAll()
 
                         .requestMatchers(
